02e8ce97138928e2e77ac6c35b279d3408293658
f.aljutaili <f.aljutaili@tamkeentech.sa>
2023-11-28 08:36:33 +0300

    will this work?

--- lua/laravelTinker/init.lua
+++ lua/laravelTinker/init.lua
@@ -1,5 +1,28 @@
+local M = {}
 
-vim.cmd[[set runtimepath+=~/.config/nvim]]
 
-vim.api.nvim_command("command! RunInTinker lua require('laravelTinker.runInTinker').runInTinker()")
+function M.run_laravel_tinker()
+    local file_path = vim.fn.expand('%:p')
+    local command = string.format('php artisan tinker < %s', file_path)
+    local output = vim.fn.system(command)
+
+    local float_opts = {
+        relative = 'cursor',
+        width = 0.8,
+        height = 0.8,
+        row = 1, col = 1,
+        border = 'single',
+    }
+
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.fn.split(output, '\n'))
+    local win_id = vim.api.nvim_open_win(bufnr, true, float_opts)
+
+    return {
+        bufnr = bufnr,
+        win_id = win_id,
+    }
+end
+
+return M
 

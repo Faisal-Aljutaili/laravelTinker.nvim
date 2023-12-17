2ce6e22d16a733580a54e3c226b35cf642152b55
f.aljutaili <f.aljutaili@tamkeentech.sa>
2023-12-17 12:03:17 +0300

    adding run file in tinker

--- lua/laravelTinker/init.lua
+++ lua/laravelTinker/init.lua
@@ -1,5 +1,53 @@
 local M = {}
 
+function M.run_file_in_laravel_tinker()
+	-- Get all lines from the current buffer
+	local file_content = vim.fn.getline(1, "$")
+	local content = table.concat(file_content, "\n")
+
+	-- Create a temporary file to store the content
+	local temp_file = vim.fn.tempname()
+	local temp_file_cmd = temp_file .. ".php"
+
+	-- Write the content to the temporary file
+	vim.fn.writefile({ content }, temp_file_cmd)
+
+	local command = string.format("php artisan tinker < %s", vim.fn.shellescape(temp_file_cmd))
+	local output = vim.fn.system(command)
+
+	-- Remove the temporary file
+	vim.fn.delete(temp_file_cmd)
+
+	local float_opts = {
+		relative = "cursor",
+		width = math.floor(vim.fn.winwidth(0) * 0.8),
+		height = math.floor(vim.fn.winheight(0) * 0.8),
+		row = 1,
+		col = 1,
+		border = "single",
+	}
+
+	local bufnr = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(output, "\n"))
+
+	local win_id = vim.api.nvim_open_win(bufnr, true, float_opts)
+
+	-- Set up mappings to close the window
+	local close_mappings = {
+		["q"] = "<Cmd>q<CR>",
+		["<Esc>"] = "<Cmd>q<CR>",
+	}
+
+	for key, mapping in pairs(close_mappings) do
+		vim.api.nvim_buf_set_keymap(bufnr, "n", key, mapping, { noremap = true, silent = true })
+	end
+
+	return {
+		bufnr = bufnr,
+		win_id = win_id,
+	}
+end
+
 function M.run_laravel_tinker()
 	-- Get all lines from the current buffer
 	local file_content = vim.fn.getline(1, "$")
